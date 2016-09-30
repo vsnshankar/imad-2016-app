28022c28f6c8a046e82563598facb4df0d2c4db4
vsnshankar <venkatesanShankaranarayanan@gmail.com>
2016-09-30 22:07:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.get('/', function (req, res) {
 app.get('/article-one', function(req,res) {
 res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 } );  
-app.get('/article-one', function(req,res) {
+app.get('/article-two', function(req,res) {
 res.send('Article two requested and will be served here');
 } );  
-app.get('/article-one', function(req,res) {
+app.get('/article-three', function(req,res) {
 res.send('Article three requested and will be served here');
 } );  
 
